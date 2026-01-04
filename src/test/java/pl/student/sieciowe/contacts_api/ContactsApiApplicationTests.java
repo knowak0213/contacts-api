81bbbda964e9bae2b67b0c6629ee687e72de3625
Karol Nowak <02karoln@gmail.com>
2026-01-04 20:30:22 +0100

    Rest API - Lista kontaktow

---
+++ src/test/java/pl/student/sieciowe/contacts_api/ContactsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.student.sieciowe.contacts_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
